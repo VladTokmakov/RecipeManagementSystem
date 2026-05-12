60eece1aaa9638fbc4f4c99e5fe17bf21033c888
VladTokmakov <vlad.tokmakov.07@mail.ru>
2026-05-12 19:38:42 +0300

    Implementing OAuth2 security using Keycloak integration

--- catalogue-service/src/main/java/com/vlad/catalogue/controller/ProductsRestController.java
+++ catalogue-service/src/main/java/com/vlad/catalogue/controller/ProductsRestController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
--- manager-app/src/main/java/com/vlad/managerapp/config/ClientBeans.java
+++ manager-app/src/main/java/com/vlad/managerapp/config/ClientBeans.java
@@ -1,9 +1,13 @@
 package com.vlad.managerapp.config;
 
 import com.vlad.managerapp.client.RestClientProductsRestClient;
+import com.vlad.managerapp.security.OAuthClientHttpRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,9 +15,16 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClient productsRestClient(
-            @Value("${vlad.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
+            @Value("${vlad.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${vlad.services.catalogue.registration-id:keycloak}") String registrationId) {
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                        authorizedClientRepository), registrationId))
                 .build());
     }
 }
